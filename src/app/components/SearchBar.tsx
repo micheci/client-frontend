af9833d2a39ccfb605b21609b21dde9e1fb6a122
micheci <michecimartinez@gmail.com>
2025-03-14 16:25:32 -0500

    adding search bar

---
+++ src/app/components/SearchBar.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React, { useState } from "react";
+//import { useRouter } from "next/router"; // Import Next.js Router
+
+const SearchBar = () => {
+  const [query, setQuery] = useState("");
+  //const router = useRouter(); // Initialize Next.js router
+
+  const handleSearch = () => {
+    if (query) {
+      // Redirect to /properties page with the query parameter
+      console.log(query, "query from search");
+      //router.push(`/properties?search=${query}`);
+    }
+  };
+
+  return (
+    <div className="relative z-10 mx-auto w-full max-w-3xl -mt-10 bg-white p-4 shadow-lg rounded-lg flex items-center">
+      <input
+        type="text"
+        placeholder="Search by address, city, or ZIP code..."
+        className="flex-grow p-2 border rounded-l-lg outline-none"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <button
+        onClick={handleSearch}
+        className="bg-blue-500 text-white px-4 py-2 rounded-r-lg hover:bg-blue-600"
+      >
+        Search
+      </button>
+    </div>
+  );
+};
+
+export default SearchBar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Hero from "../app/components/Hero";
 import Navbar from "./components/Navbar";
 import PropertyGrid from "./components/PropertyGrid";
+import SearchBar from "./components/SearchBar";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <div className="relative">
+        <SearchBar />
+      </div>
       <PropertyGrid />
     </>
   );
